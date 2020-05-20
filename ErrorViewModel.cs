6748dbc0db58ec4e70892d22ddd292a23db6d3cc
ponomarichka <61557620+ponomarichka@users.noreply.github.com>
2020-05-20 22:37:15 +0300

    Add files via upload

---
+++ ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Laba1_ISTTP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
